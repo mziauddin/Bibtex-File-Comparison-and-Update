a1a1acf87bb5fa275490ab73b2df85a912d94702
Mohammed Ziauddin <mdziauddin1988@gmail.com>
2015-12-13 18:20:32 -0600

    Adding requirements file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 #(relative to the 'root' dir, where setup.py is)
 #You could be more specific.
 
-setup(name='Bibtex_File_Comparison_and_Update',
+setup(name='Bibtex_File_Comparison',
       version='1.0',
       description='This application aims to compare two bibtex files and help the user in keeping bibtex records in sync on both the files',
       # url='http://github.com/storborg/funniest',
